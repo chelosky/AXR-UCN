8962db40d8fd3d471070b896b17bc6f976fa0d69
Marcelo Lazo <marcelolazo19972013@hotmail.com>
2019-08-20 22:46:15 -0400

    Fix bugs of slide 2

--- Assets/Scripts/CardBehaviour.cs
+++ Assets/Scripts/CardBehaviour.cs
@@ -6,19 +6,25 @@ public class CardBehaviour : MonoBehaviour
 {
     public AcademicosModel academicModel; 
     private Animator m_Animator;
+    private List<GameObject> listButtonsLinks = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
         if(this.academicModel != null){
             // Debug.Log(this.transform.childCount);
             // this.transform.GetChild(1).GetComponent<Image>().sprite = academicModel.fotoPerfil;
-            this.transform.Find("Image").GetComponent<Image>().sprite = academicModel.fotoPerfil;
-            this.transform.Find("Nombre").GetComponent<TMPro.TextMeshProUGUI>().text = academicModel.name;
-            this.transform.Find("Grado").GetComponent<TMPro.TextMeshProUGUI>().text = academicModel.grado;
-            this.transform.Find("Email").GetComponent<TMPro.TextMeshProUGUI>().text = academicModel.email;
+            this.transform.Find("Elements").Find("Image").GetComponent<Image>().sprite = academicModel.fotoPerfil;
+            this.transform.Find("Elements").Find("Nombre").GetComponent<TMPro.TextMeshProUGUI>().text = academicModel.name;
+            this.transform.Find("Elements").Find("Grado").GetComponent<TMPro.TextMeshProUGUI>().text = academicModel.grado;
+            this.transform.Find("Elements").Find("Email").GetComponent<TMPro.TextMeshProUGUI>().text = academicModel.email;
         }
         m_Animator = gameObject.GetComponent<Animator>();
         this.GetComponent<Button>().onClick.AddListener( () => {TriggerPressed();});
+        listButtonsLinks.Add(this.transform.Find("GS").gameObject);
+        listButtonsLinks.Add(this.transform.Find("External").gameObject);
+        listButtonsLinks.Add(this.transform.Find("RG").gameObject);
+        this.transform.Find("BACK").GetComponent<Button>().onClick.AddListener( () => {TriggerPressedAgain();});
+        listButtonsLinks.Add(this.transform.Find("BACK").gameObject);
     }
 
     // Update is called once per frame
@@ -33,7 +39,27 @@ public class CardBehaviour : MonoBehaviour
 
     private IEnumerator TriggerButton(){
         m_Animator.SetTrigger("Pressed");
-        yield return new WaitForSeconds(0.2f);
+        m_Animator.SetBool("Animating",true);
+        yield return new WaitForSeconds(0.5f);
         m_Animator.ResetTrigger("Pressed");
+        for(int i=0;i<this.listButtonsLinks.Count;i++){
+            this.listButtonsLinks[i].GetComponent<Animator>().SetTrigger("FadeIn");
+        }
+        yield return new WaitForSeconds(0.5f);
+    }
+
+    public void TriggerPressedAgain(){
+        StartCoroutine(this.BackToNormal());
+    }
+
+    private IEnumerator BackToNormal(){
+        m_Animator.SetBool("Animating",true);
+        for(int i=0;i<this.listButtonsLinks.Count;i++){
+            this.listButtonsLinks[i].GetComponent<Animator>().SetTrigger("FadeOut");
+        }  
+        yield return new WaitForSeconds(0.2f);
+        m_Animator.SetBool("Animating",false);
+
     }
 }
+
