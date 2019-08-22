8543692f9f8fea010d0028de4446c483d73646e3
Marcelo Lazo <marcelolazo19972013@hotmail.com>
2019-08-22 18:34:06 -0400

    Merge Jerson Panel (ICCI)

---
+++ Assets/Scripts/InfoICCI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InfoICCI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Panel1;
+    public GameObject Panel2;
+    public GameObject Panel3;
+    public void openPanel(){
+        if(Panel1 != null && Panel2!=null &&Panel3!=null)
+        {
+            Panel1.SetActive(true);
+            Panel2.SetActive(false);
+            Panel3.SetActive(false);
+        }
+    }
+
+   
+     public void openPanel3(){
+         if(Panel1 != null && Panel2!=null &&Panel3!=null)
+        {
+            Panel1.SetActive(false);
+            Panel2.SetActive(false);
+            Panel3.SetActive(true);
+        }
+        
+    }
+    public void openPanel2(){
+         if(Panel1 != null && Panel2!=null &&Panel3!=null)
+        {
+            Panel1.SetActive(false);
+            Panel2.SetActive(true);
+            Panel3.SetActive(false);
+        }
+    }
+    void Start()
+    {
+     Panel1.SetActive(true);
+     Panel2.SetActive(false);
+     Panel3.SetActive(false);
+
+    }
+
+  
+}
